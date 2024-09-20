11c7c7933d04e362f057f37a8df0fe25a2ffeaa3
thilinam <thilinamanawadu@gmail.com>
2024-09-20 18:33:57 +0530

    adding validation

--- models/user.model.js
+++ models/user.model.js
@@ -5,23 +5,27 @@ const userSchema = mongoose.Schema({
 
         id:{
             type:String,
-            required:[true,"Unique Identity"]
+            required:[true,"Unique Identity"],
+            trim:true
         },
 
         firstname:{
             type:String,
-            required:[true,"First Name"]
+            required:[true,"First Name"],
+            trim:true
         },
 
         lastname:{
             type:String,
-            required:[true,"Last Name"]
+            required:[true,"Last Name"],
+            trim:true
         },
 
         passportno:{
             type:String,
             required:false,
-            default:'NPM-00000'
+            default:'NPM-00000',
+            trim:true
         }
 },
 {timestamp:true})
